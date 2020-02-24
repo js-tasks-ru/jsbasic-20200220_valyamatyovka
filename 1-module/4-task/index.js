497c01ed7d810fd5853efa9eadd694c10d594a57
Valentyna Izghanych <v.izghanych@astoundcommerce.com>
2020-02-24 15:24:27 +0200

    2 module 4d task done

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,11 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+  let name = str.toLowerCase();
+			
+  if (name.includes('1xbet') || name.includes('xxx')) {
+    return true;
+  } else {
+    return false;
+  }
 }
