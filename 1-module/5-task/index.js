50d63e5abb403e3bfc7b09d8906a1606c5441fae
Valentyna Izghanych <v.izghanych@astoundcommerce.com>
2020-02-24 15:44:26 +0200

    2d module 5th task done

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,10 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  if (str.length > maxlength) {
+    let newStr = str.slice(0, maxlength - 1) + "…";
+    return newStr;
+  } else {
+    return str;
+  }
 }
