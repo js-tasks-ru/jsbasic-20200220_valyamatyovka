35361e89fe76e2605312c7b547b25f5f7e311e6b
Valentyna Izghanych <v.izghanych@astoundcommerce.com>
2020-03-06 14:02:46 +0200

    3 module 2nd task done

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,32 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let newString = str.replace(/[,\/#!$%\^&\*;:{}=+\_`~()^A-Za-zА-Яа-яЁё\s]/g," ");
+  let arr = newString.split(' '); 
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i];
+  }
+
+  let min = arrayMin(arr);
+  let max = arrayMax(arr);
+
+  function arrayMin(arr) {
+    return arr.reduce(function (min, max) {
+      return ( min < max ? min : max );
+    });
+  }
+
+  function arrayMax(arr) {
+    return arr.reduce(function (min, max) {
+      return ( min > max ? min : max );
+    });
+  }
+
+  let result = {
+    min: min,
+    max: max
+  };
+  
+  return result;
 }
