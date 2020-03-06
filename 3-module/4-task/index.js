1f8154d0f61a607e278498ede5cea599f48ec9fd
Valentyna Izghanych <v.izghanych@astoundcommerce.com>
2020-03-06 14:35:39 +0200

    3 module 4th task done

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,4 +3,11 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
+  let arr = [];
+		  
+  for (let i = 0; i < users.length; i++) {
+  arr.push(users[i].name);
+  }
+  
+  return arr;
 }
