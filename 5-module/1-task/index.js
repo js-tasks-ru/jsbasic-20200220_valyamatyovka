ca37b20043b65b3ceb9cececaff5f0d79739c4aa
Valentyna Izghanych <v.izghanych@astoundcommerce.com>
2020-03-17 16:54:36 +0200

    5th module 1st task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,31 @@
  * @param {Element} table
  */
 function highlight(table) {
+  let cells = table.querySelectorAll('td');
+
+  for (let i = 0; i < cells.length; i++) {
+    if (!cells[i].hasAttribute("data-available")) {
+      cells[i].closest('tr').setAttribute('hidden', '');
+    }
+
+    if (cells[i].hasAttribute("data-available")) {
+      if (cells[i].dataset.available === 'true') {
+        cells[i].closest('tr').classList.add('available');
+      } else {
+        cells[i].closest('tr').classList.add('unavailable');
+      }
+    }
+
+    if (cells[i].innerHTML === 'm') {
+      cells[i].closest('tr').classList.add('male');
+    }
+
+    if (cells[i].innerHTML === 'f') {
+      cells[i].closest('tr').classList.add('female');
+    }
+
+    if (cells[i].innerHTML < 18) {
+      cells[i].closest('tr').style.textDecoration = "line-through";
+    }
+  }
 }
